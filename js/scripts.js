7ea559911ce92634df3c8ef0fc463d813e2e7e35
jharlow34 <64202161+jharlow34@users.noreply.github.com>
2020-06-15 11:40:39 +0100

    Updated css, html, js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$("#search").on("keyup", function() {
+    const value = $(this)
+        .val().toLowerCase();
+    $(".grid a").filter(function() {
+        $(this).toggle($(this).attr("data-title").toLowerCase().indexOf(value) > -1
+        );
+    });
+});
+
